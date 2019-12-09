b03eb74d81899d6883a9f6d458cd982efeca7e63
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-09 23:00:56 +0000

    allmaze2

--- allmaze.js
+++ allmaze.js
@@ -1,8 +1,4 @@
-let mySmallMaze = [
-  [' ', ' ', ' '],
-  [' ', '*', ' '],
-  [' ', ' ', 'e']
-];
+
 
 let maze = [
   [' ', ' ', ' ', '*', ' ', ' ', ' '],
@@ -12,7 +8,7 @@ let maze = [
   [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-function out(maze, x, y, currPath, allPaths, tempMaze, base=false) {
+function out(maze, x, y, currPath, allPaths, tempMaze) {
   // console.log(maze[0]);
   // console.log(maze[1]);
   // console.log(maze[2]);
@@ -21,35 +17,35 @@ function out(maze, x, y, currPath, allPaths, tempMaze, base=false) {
   // console.log('\b');
   if(tempMaze[y][x] === ' '){
     tempMaze[y][x] = '1';
-      if (x + 1 < tempMaze[0].length && (tempMaze[y][x+1] === ' ' || tempMaze[y][x+1] ===  'e')) {
-        currPath.push('R')
-        if(!out(tempMaze, x+1, y, currPath, allPaths, tempMaze)){
-          currPath.pop();
-          tempMaze[y][x+1] = ' '
-        }
+    if (x + 1 < tempMaze[0].length && (tempMaze[y][x+1] === ' ' || tempMaze[y][x+1] ===  'e')) {
+      currPath.push('R');
+      if(!out(tempMaze, x+1, y, currPath, allPaths, tempMaze)){
+        currPath.pop();
+        tempMaze[y][x+1] = ' ';
       }
-      if (x > 0 && (tempMaze[y][x-1] === ' ' ||tempMaze[y][x-1] ===  'e')){
-        currPath.push('L')
-        if(!out(tempMaze, x-1, y, currPath, allPaths, tempMaze)){
-          currPath.pop();
-          tempMaze[y][x-1] = ' '
-        }
+    }
+    if (x > 0 && (tempMaze[y][x-1] === ' ' ||tempMaze[y][x-1] ===  'e')){
+      currPath.push('L');
+      if(!out(tempMaze, x-1, y, currPath, allPaths, tempMaze)){
+        currPath.pop();
+        tempMaze[y][x-1] = ' ';
       }
-      if (y + 1 < tempMaze.length && (tempMaze[y+1][x] === ' ' ||tempMaze[y+1][x] ===  'e')) {
-        currPath.push('D')
-        if(!out(tempMaze, x, y+1, currPath, allPaths, tempMaze)){
-          currPath.pop();
-          tempMaze[y+1][x] = ' '
-        }
+    }
+    if (y + 1 < tempMaze.length && (tempMaze[y+1][x] === ' ' ||tempMaze[y+1][x] ===  'e')) {
+      currPath.push('D');
+      if(!out(tempMaze, x, y+1, currPath, allPaths, tempMaze)){
+        currPath.pop();
+        tempMaze[y+1][x] = ' ';
       }
-      if (y > 0 && (tempMaze[y-1][x] === ' ' || tempMaze[y-1][x] === 'e')) {
-        currPath.push('U')
-        if(!out(tempMaze, x, y-1, currPath, allPaths, tempMaze)){
-          currPath.pop();
-          tempMaze[y-1][x] = ' '
-          return false;
-        }
+    }
+    if (y > 0 && (tempMaze[y-1][x] === ' ' || tempMaze[y-1][x] === 'e')) {
+      currPath.push('U');
+      if(!out(tempMaze, x, y-1, currPath, allPaths, tempMaze)){
+        currPath.pop();
+        tempMaze[y-1][x] = ' ';
+        return false;
       }
+    }
   } else if (tempMaze[y][x] === 'e'){
     allPaths.push([...currPath]);
     currPath.pop();
@@ -60,4 +56,4 @@ function out(maze, x, y, currPath, allPaths, tempMaze, base=false) {
 
 }
 
-console.log(out(maze, 0, 0, [], [], maze))
+console.log(out(maze, 0, 0, [], [], maze));
